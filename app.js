94ce624f58ffb5e4a8749665e6327c077009b234
Amr A. Abdelmoez <amradel88@live.com>
2018-05-26 16:00:56 +0200

    feat: PWA

--- app.js
+++ app.js
@@ -214,3 +214,14 @@ const pomodoroContorller = (function(model, view) {
 })(pomodoroModel, pomodoroView);
 
 pomodoroContorller.init();
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log('Service worker registration succeeded:', registration);
+    }).catch(function(error) {
+      console.log('Service worker registration failed:', error);
+    });
+  } else {
+    console.log('Service workers are not supported.');
+  }
